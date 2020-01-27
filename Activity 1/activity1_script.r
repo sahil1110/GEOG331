4a9fe2b3ac650d1226ff179ec28a170d6f80433e
Sahil Lalwani '22 <slalwani@colgate.edu>
2020-01-27 10:29:57 -0500

    Added practice operations to R script

--- Activity 1/activity1_script.r
+++ Activity 1/activity1_script.r
@@ -1,14 +1,35 @@
 # Activity 1
-print("hello R world")
+print("hello R world") # Output: "hello R world"
 
-#three multiplied by 3
-3*3
+#three multiplied by three
+3*3 #output: 9
 
 #four plus four
-4+4
+4+4 #output: 8
 
-#6 raised to the power 3
-6^3
+#six raised to the power three
+6^3 #output: 216
 
 a <- 4527379383939
-a*100
+a*100 #output: 4.527379e+14
+
+#six raised to the power six
+6^6 #output: 46656 
+
+#add five and two hundred ten
+5+210 #output: 215
+
+#three minus 10
+3-10 #output: -7
+
+b<-2446
+5*b #output: 12230
+
+samplecal<-c(23,456,765,1,2,8,65)
+(5*samplecal)+2
+# output: 117 2282 3827 7 12 42 327
+
+m<-c(11,290848, samplecal)
+# m<- c(11, 290848, 23,456,765,1,2,8,65)
+m*2
+#Final output: 22 581696 46 912 1530 2 4 16 130
