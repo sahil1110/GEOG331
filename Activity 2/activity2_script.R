001d1f6db29063878fd287a37294c049c816351e
Sahil Lalwani <slalwani@colgate.edu>
2020-02-04 00:01:14 -0500

    Made progress on Activity 2 until Question 5

--- Activity 2/activity2_script.R
+++ Activity 2/activity2_script.R
@@ -56,23 +56,14 @@ averageTemp
 #you will have to reference the level output or look at the row of data to see the character designation.
 datW$siteN <- as.numeric(datW$NAME)
 
-#make a histogram for the first site in our levels
-#main= is the title name argument.
-#Here you want to paste the actual name of the factor not the numeric index
-#since that will be more meaningful. 
-hist(datW$TAVE[datW$siteN == 1],
-     freq=FALSE, 
-     main = paste(levels(datW$NAME)[1]),
-     xlab = "Average daily temperature (degrees C)", 
-     ylab="Relative frequency",
-     col="grey50",
-     border="white")
+## Question 4
 
+par(mfrow= c(2,2))
 #make a histogram for the first site in our levels, Aberdeen
 #main= is the title name argument.
 #Here you want to paste the actual name of the factor not the numeric index
 #since that will be more meaningful. 
-hist(datW$TAVE[datW$siteN == 1],
+h1<-hist(datW$TAVE[datW$siteN == 1],
      freq=FALSE, 
      main = paste(levels(datW$NAME)[1]),
      xlab = "Average daily temperature (degrees C)", 
@@ -96,3 +87,238 @@ abline(v = mean(datW$TAVE[datW$siteN == 1],na.rm=TRUE) + sd(datW$TAVE[datW$siteN
        col = "tomato3", 
        lty = 3,
        lwd = 3)
+
+## Site 2
+
+#make a histogram for the second site in our levels, Livermore
+#main= is the title name argument.
+#Here you want to paste the actual name of the factor not the numeric index
+#since that will be more meaningful. 
+h2<-hist(datW$TAVE[datW$siteN == 2],
+     freq=FALSE, 
+     main = paste(levels(datW$NAME)[2]),
+     xlab = "Average daily temperature (degrees C)", 
+     ylab="Relative frequency",
+     col="black",
+     border="white")
+#add mean line with red (tomato3) color
+#and thickness of 3
+abline(v = mean(datW$TAVE[datW$siteN == 2],na.rm=TRUE), 
+       col = "tomato3",
+       lwd = 3)
+#add standard deviation line below the mean with red (tomato3) color
+#and thickness of 3
+abline(v = mean(datW$TAVE[datW$siteN == 2],na.rm=TRUE) - sd(datW$TAVE[datW$siteN == 2],na.rm=TRUE), 
+       col = "tomato3", 
+       lty = 3,
+       lwd = 3)
+#add standard deviation line above the mean with red (tomato3) color
+#and thickness of 3
+abline(v = mean(datW$TAVE[datW$siteN == 2],na.rm=TRUE) + sd(datW$TAVE[datW$siteN == 2],na.rm=TRUE), 
+       col = "tomato3", 
+       lty = 3,
+       lwd = 3)
+
+## Site 3
+
+#make a histogram for the third site in our levels, Mandan Experiment Station
+#main= is the title name argument.
+#Here you want to paste the actual name of the factor not the numeric index
+#since that will be more meaningful. 
+h3<-hist(datW$TAVE[datW$siteN == 3],
+     freq=FALSE, 
+     main = paste(levels(datW$NAME)[3]),
+     xlab = "Average daily temperature (degrees C)", 
+     ylab="Relative frequency",
+     col="blue",
+     border="white")
+#add mean line with red (tomato3) color
+#and thickness of 3
+abline(v = mean(datW$TAVE[datW$siteN == 3],na.rm=TRUE), 
+       col = "tomato3",
+       lwd = 3)
+#add standard deviation line below the mean with red (tomato3) color
+#and thickness of 3
+abline(v = mean(datW$TAVE[datW$siteN == 3],na.rm=TRUE) - sd(datW$TAVE[datW$siteN == 2],na.rm=TRUE), 
+       col = "tomato3", 
+       lty = 3,
+       lwd = 3)
+#add standard deviation line above the mean with red (tomato3) color
+#and thickness of 3
+abline(v = mean(datW$TAVE[datW$siteN == 3],na.rm=TRUE) + sd(datW$TAVE[datW$siteN == 2],na.rm=TRUE), 
+       col = "tomato3", 
+       lty = 3,
+       lwd = 3)
+
+## Site 4
+
+#make a histogram for the second site in our levels, Mormon Flat
+#main= is the title name argument.
+#Here you want to paste the actual name of the factor not the numeric index
+#since that will be more meaningful. 
+h4<-hist(datW$TAVE[datW$siteN == 4],
+     freq=FALSE, 
+     main = paste(levels(datW$NAME)[4]),
+     xlab = "Average daily temperature (degrees C)", 
+     ylab="Relative frequency",
+     col="lightgray",
+     border="white")
+#add mean line with red (tomato3) color
+#and thickness of 3
+abline(v = mean(datW$TAVE[datW$siteN == 4],na.rm=TRUE), 
+       col = "tomato3",
+       lwd = 3)
+#add standard deviation line below the mean with red (tomato3) color
+#and thickness of 3
+abline(v = mean(datW$TAVE[datW$siteN == 4],na.rm=TRUE) - sd(datW$TAVE[datW$siteN == 2],na.rm=TRUE), 
+       col = "tomato3", 
+       lty = 3,
+       lwd = 3)
+#add standard deviation line above the mean with red (tomato3) color
+#and thickness of 3
+abline(v = mean(datW$TAVE[datW$siteN == 4],na.rm=TRUE) + sd(datW$TAVE[datW$siteN == 2],na.rm=TRUE), 
+       col = "tomato3", 
+       lty = 3,
+       lwd = 3)
+
+
+## Question 5
+# the seq function generates a sequence of numbers that we can use to plot the normal across the range of temperature
+# values
+
+dev.off()
+par(mfrow=c(2,2))
+
+h1<-hist(datW$TAVE[datW$siteN == 1],
+         freq=FALSE, 
+         main = paste(levels(datW$NAME)[1]),
+         xlab = "Average daily temperature (degrees C)", 
+         ylab="Relative frequency",
+         col="grey50",
+         border="white")
+
+x.plot <- seq(-10,30, length.out = 100)
+#the dnorm function will produce the probability density based on a mean and standard deviation.
+
+y.plot <-  dnorm(seq(-10,30, length.out = 100),
+                 mean(datW$TAVE[datW$siteN == 1],na.rm=TRUE),
+                 sd(datW$TAVE[datW$siteN == 1],na.rm=TRUE))
+
+#create a density that is scaled to fit in the plot since the density has a different range from the data density.
+#!!! this is helpful for putting multiple things on the same plot
+#!!! It might seem confusing at first. It means the maximum value of the plot is always the same between the two
+# datasets on the plot. Here both plots share zero as a minimum.
+
+y.scaled <- (max(h1$density)/max(y.plot)) * y.plot
+
+#points function adds points or lines to a graph
+#the first two arguments are the x coordinates and the y coordinates.
+
+points(x.plot,
+       y.scaled, 
+       type = "l", 
+       col = "royalblue3",
+       lwd = 4, 
+       lty = 2)
+
+## Site 2
+
+h2<-hist(datW$TAVE[datW$siteN == 2],
+         freq=FALSE, 
+         main = paste(levels(datW$NAME)[2]),
+         xlab = "Average daily temperature (degrees C)", 
+         ylab="Relative frequency",
+         col="grey50",
+         border="white")
+
+x.plot <- seq(-10,40, length.out = 100)
+#the dnorm function will produce the probability density based on a mean and standard deviation.
+
+y.plot <-  dnorm(seq(-10,40, length.out = 100),
+                 mean(datW$TAVE[datW$siteN == 2],na.rm=TRUE),
+                 sd(datW$TAVE[datW$siteN == 2],na.rm=TRUE))
+
+#create a density that is scaled to fit in the plot since the density has a different range from the data density.
+#!!! this is helpful for putting multiple things on the same plot
+#!!! It might seem confusing at first. It means the maximum value of the plot is always the same between the two
+# datasets on the plot. Here both plots share zero as a minimum.
+
+y.scaled <- (max(h2$density)/max(y.plot)) * y.plot
+
+#points function adds points or lines to a graph
+#the first two arguments are the x coordinates and the y coordinates.
+
+points(x.plot,
+       y.scaled, 
+       type = "l", 
+       col = "royalblue3",
+       lwd = 4, 
+       lty = 2)
+
+## Site 3
+
+h3<-hist(datW$TAVE[datW$siteN == 3],
+         freq=FALSE, 
+         main = paste(levels(datW$NAME)[3]),
+         xlab = "Average daily temperature (degrees C)", 
+         ylab="Relative frequency",
+         col="grey50",
+         border="white")
+
+x.plot <- seq(-40,40, length.out = 100)
+#the dnorm function will produce the probability density based on a mean and standard deviation.
+
+y.plot <-  dnorm(seq(-40,40, length.out = 100),
+                 mean(datW$TAVE[datW$siteN == 3],na.rm=TRUE),
+                 sd(datW$TAVE[datW$siteN == 3],na.rm=TRUE))
+
+#create a density that is scaled to fit in the plot since the density has a different range from the data density.
+#!!! this is helpful for putting multiple things on the same plot
+#!!! It might seem confusing at first. It means the maximum value of the plot is always the same between the two
+# datasets on the plot. Here both plots share zero as a minimum.
+
+y.scaled <- (max(h3$density)/max(y.plot)) * y.plot
+
+#points function adds points or lines to a graph
+#the first two arguments are the x coordinates and the y coordinates.
+
+points(x.plot,
+       y.scaled, 
+       type = "l", 
+       col = "royalblue3",
+       lwd = 4, 
+       lty = 2)
+
+## Site 4
+
+h4<-hist(datW$TAVE[datW$siteN == 4],
+         freq=FALSE, 
+         main = paste(levels(datW$NAME)[4]),
+         xlab = "Average daily temperature (degrees C)", 
+         ylab="Relative frequency",
+         col="grey50",
+         border="white")
+
+x.plot <- seq(-40,40, length.out = 100)
+#the dnorm function will produce the probability density based on a mean and standard deviation.
+
+y.plot <-  dnorm(seq(-40,40, length.out = 100),
+                 mean(datW$TAVE[datW$siteN == 4],na.rm=TRUE),
+                 sd(datW$TAVE[datW$siteN == 4],na.rm=TRUE))
+
+#create a density that is scaled to fit in the plot since the density has a different range from the data density.
+#!!! this is helpful for putting multiple things on the same plot
+#!!! It might seem confusing at first. It means the maximum value of the plot is always the same between the two
+# datasets on the plot. Here both plots share zero as a minimum.
+
+y.scaled <- (max(h4$density)/max(y.plot)) * y.plot
+
+#points function adds points or lines to a graph
+#the first two arguments are the x coordinates and the y coordinates.
+
+points(x.plot,
+       y.scaled, 
+       type = "l", 
+       col = "royalblue3",
+       lwd = 4, 
+       lty = 2)
